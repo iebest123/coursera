467e8d96a0d29c7f51a432b24b72d85c486115ed
iebest123 <98957248+iebest123@users.noreply.github.com>
2022-03-05 11:38:35 +0500

    website

---
+++ front.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+  <title>Week 3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+  <style>
+   div {
+  background-color: lightgrey;
+  width: 300px;
+  border: 3px solid black;
+  padding: 10px;
+  margin: 10px;
+}
+  </style>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Food, LLC</a>
+    </div>
+   
+  </div>
+</nav>
+
+  <!--
+<div class="container">
+  <h3>Inverted Navbar</h3>
+  <p>An inverted navbar is black instead of gray.</p>
+</div>
+-->
+<center>
+ 
+  <h2><b>Our Menu</b></h2>
+ 
+    <div>
+    Imagine each paragraph as having three general layers of text. The core content is in the middle. It includes all the evidence you need to make the point. However, this evidence needs to be introduced by a topic sentence in some way or your readers don't know what to do with all the evidence you have given them. Therefore, the beginning of the paragraph explains the controlling idea of the paragraph. The last part of the paragraph tells the reader how the paragraph relates to the broader argument and often provides a transition to the next idea. Once you have mastered the use of topic sentences, you may decide that the topic sentence for a particular paragraph really should not be the first sentence of the paragraph. This is fine—the topic sentence can actually go at the beginning, middle, or end of a paragraph; what's important is that it is there to inform readers what the main idea of the paragraph is and how it relates back to the broader thesis of your paper.The paragraph has more than one controlling idea. This is the most common reason why a paragraph is too long. If a paragraph is more than a page long, it likely contains more than one controlling idea. In this case, consider eliminating sentences that relate to the second idea, with the thought that maybe they don't really inform and help support the central research problem, or split the paragraph into two or more paragraphs, each with only one controlling idea.
+    </div>
+
+</center>
+</body>
+</html>
